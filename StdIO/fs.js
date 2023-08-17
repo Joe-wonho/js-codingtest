7dcc194aca17ff7f715702e031248e83de9664b4
Joe-wonho <whdnjsgh22@naver.com>
2023-08-17 19:26:16 +0900

    2525 알람시계문제

--- StdIO/fs.js
+++ StdIO/fs.js
@@ -6,4 +6,15 @@ let input = fs
   .toString()
   .split('\n');
 
-console.log(input); // ["5", "17 88 53 100 73"]
+console.log(input);
+
+// https://www.acmicpc.net/problem/2525
+let [a, b] = input[0].split(' ').map(Number);
+let c = Number(input[1]);
+
+let totalMinute = a * 60 + b + c;
+totalMinute %= 1440;
+let hour = parseInt(totalMinute / 60);
+let minute = totalMinute % 60;
+
+console.log(hour + ' ' + minute);
